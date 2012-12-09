f62a75f48a38ad8b7f81c40c54f04a04c207a825
Sivan Greenberg <sivan@omniqueue.com>
2012-12-09 22:40:21 +0200

    cleanup

--- server/currentaccount/models.py
+++ server/currentaccount/models.py
@@ -4,7 +4,8 @@ from djangotoolbox.fields import ListField
 MAX_CHAR_LENGTH = 20
 
 class CurrentAccount(models.Model):
-    owner = models.CharField(max_length=MAX_CHAR_LENGTH) # for a real system this'd better be a pointer to a user account.
+    # for a real system this'd better be a pointer to a user account.
+    owner = models.CharField(max_length=MAX_CHAR_LENGTH) 
     account_type = models.CharField(max_length=MAX_CHAR_LENGTH)
     balance = models.FloatField(default=0)
 
