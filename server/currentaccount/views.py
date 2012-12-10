865d567b34488b7c9defc5876a46df611121483e
Sivan Greenberg <sivan@omniqueue.com>
2012-12-11 00:49:44 +0200

    added the view with the pubnub cred place holders

--- server/currentaccount/views.py
+++ server/currentaccount/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from currentaccount.models import CurrentAccount
 from django.contrib.auth.decorators import login_required
+from django.shortcut import render
 
 def index(request):
     return HttpResponse("Welcome to Bank Current Account simulation for PubNub AES encrypted real time notifications.")
@@ -9,4 +10,5 @@ def index(request):
 @login_required
 def detail(request):
     curaccount = CurrentAccount.objects.get(owner=request.user)
-    return HttpResponse("Balance for your (" + request.user.username + ") account: " + unicode(curaccount.balance))
+    context = {'username' : request.user.username, 'balance' : unicode(curaccount.balance)}
+    return render(request, 'currentaccount/detail.html')
